0a83c31a6c06dbaa8d62db73a8a47ef5213b1f2e
Anthony Giuliano <agiuliano@squareup.com>
2022-11-09 10:41:57 -0500

    fix spacing

--- src/routes/api/payment/+server.js
+++ src/routes/api/payment/+server.js
@@ -3,31 +3,31 @@ import { Client } from 'square';
 import { randomUUID } from 'crypto';
 
 BigInt.prototype.toJSON = function () {
-	return this.toString();
+  return this.toString();
 };
 
 const { paymentsApi } = new Client({
-	accessToken: import.meta.env.VITE_SQUARE_ACCESS_TOKEN,
-	environment: 'sandbox'
+  accessToken: import.meta.env.VITE_SQUARE_ACCESS_TOKEN,
+  environment: 'sandbox'
 });
 
 export async function POST({ request }) {
-	const { locationId, sourceId } = await request.json();
-	console.log(locationId, sourceId);
-	try {
-		const response = await paymentsApi.createPayment({
-			locationId,
-			sourceId,
-			idempotencyKey: randomUUID(),
-			amountMoney: {
-				amount: 100,
-				currency: 'USD'
-			}
-		});
-		console.log(response.result);
+  const { locationId, sourceId } = await request.json();
+  console.log(locationId, sourceId);
+  try {
+    const response = await paymentsApi.createPayment({
+      locationId,
+      sourceId,
+      idempotencyKey: randomUUID(),
+      amountMoney: {
+        amount: 100,
+        currency: 'USD'
+      }
+    });
+    console.log(response.result);
 
-		return json(response.result);
-	} catch (error) {
-		console.log(error);
-	}
+    return json(response.result);
+  } catch (error) {
+    console.log(error);
+  }
 }
